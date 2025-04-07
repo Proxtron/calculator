62794a8227dc40aa7517dc075f4a3c72c8774698
Proxtron <51761303+Proxtron@users.noreply.github.com>
2025-04-07 16:54:55 -0700

    Add Basic calculator function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,54 @@
+let operand1 = '0';
+let operator = '';
+let operand2 = '0';
+let nextOperandFlag = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const numButtons = document.querySelectorAll(".num-btn");
+    numButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            handleDigitInput(event.currentTarget.dataset.num);
+        });
+    });
+
+    const operatorButtons = document.querySelectorAll(".oper-btn");
+    operatorButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            handleOperatorInput(event.currentTarget.dataset.oper);
+        });
+    });
+
+    document.getElementById("equal-btn").addEventListener("click", handleEqualInput);
+});
+
+function handleDigitInput(digit) {
+    if(!nextOperandFlag) {
+        if(operand1 === '0') {
+            operand1 = digit + '';
+        } else {
+            operand1 += digit + '';
+        }
+        document.getElementById("num-display").innerText = operand1;
+    } else {
+        if(operand2 === '0') {
+            operand2 = digit + '';
+        } else {
+            operand2 += digit + '';
+        }
+        document.getElementById("num-display").innerText = operand2;
+    }
+}
+
+function handleOperatorInput(newOperator) {
+    operator = newOperator
+    nextOperandFlag = true;
+}
+
+function handleEqualInput() {
+   const result = operate(operator, parseInt(operand1), parseInt(operand2));
+   document.getElementById("num-display").innerText = result; 
+}
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -28,6 +79,3 @@ function operate(operator, num1, num2) {
             return 0;
     }
 }
-let operand1;
-let operator;
-let operand2;
